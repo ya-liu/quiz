cfbde12b4dea9a979f19616c0ef59075762599db
Ya Liu <sarahl0216@gmail.com>
2022-02-13 21:20:51 -0800

    added error handling for incorrect file name

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	_ = file
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }

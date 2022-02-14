d17e4185c6912f775e6f758fbbd0dfdb39230aba
Ya Liu <sarahl0216@gmail.com>
2022-02-13 22:59:20 -0800

    add timer logic to expire in timelimit and return final statement

--- main.go
+++ main.go
@@ -34,14 +34,19 @@ func main() {
 
 	correct := 0
 	for i, p := range problems {
-		select {
-		case <-timer.C:
-			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-			return
-		default:
-			fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
 			if answer == p.a {
 				fmt.Println("Correct!")
 				correct++
